8baac16a85b6c5e783281ecbf4ce661c073809eb
Vitor Gaudencio Isse de Oliveira <oliveira.vitor3@gmail.com>
2019-02-09 15:29:00 -0200

    Inicio do projeto

---
+++ MinhasNFc/MinhasNFc/Models/NFc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinhasNFc.Models
+{
+    public class NFc
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public int Serie { get; set; }
+        public string QrCode { get; set; }
+    }
+}
