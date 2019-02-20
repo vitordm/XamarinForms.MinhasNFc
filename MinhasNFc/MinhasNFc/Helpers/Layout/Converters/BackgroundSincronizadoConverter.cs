8552cde4a8330d37249cefd75f95e914af7515f3
Vitor Oliveira <vitor.oliveira@vstelecom.com.br>
2019-02-20 15:08:34 -0300

    Melhorando o layout da NFc

---
+++ MinhasNFc/MinhasNFc/Helpers/Layout/Converters/BackgroundSincronizadoConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MinhasNFc.Helpers.Layout.Converters
+{
+    class BackgroundSincronizadoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return IsSincronizado((bool)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return IsSincronizado((bool)value);
+        }
+
+        private Color IsSincronizado(bool value)
+        {
+            return value ? Color.White : Color.Gray;
+        }
+    }
+}
